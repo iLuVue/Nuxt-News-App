15a16b77a572cc7f23e9346d9442468451b2fd8c
Parsons <gongyingparsons@gmail.com>
2020-12-23 18:35:10 +0800

    build navbar

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,12 +1,25 @@
 <template>
-    <div>
-        Navbar goes here '@/components/TheNav.vue'
-    </div>
+    <b-navbar centered>
+        <template slot="start">
+            <b-navbar-item v-for="item in navbars" :key="item.title" :href="item.target">
+                {{item.title}}
+            </b-navbar-item>
+        </template>
+    </b-navbar>
 </template>
 
 <script>
     export default {
-        
+        data(){
+            return{
+                navbars : [
+                    {title:"News", target:"/"},
+                    {title:"Food & Drink", target:"/"},
+                    {title:"Events & Entertainment", target:"/"},
+                    {title:"Guides", target:"/"},
+                ]
+            }
+        }
     }
 </script>
 
