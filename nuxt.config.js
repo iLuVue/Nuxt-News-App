654d9429e134c355464b51a5f152b1be4d077adf
Khoi Phan <ngoc-khoi.phan@bollore.com>
2020-12-30 17:34:57 +0700

    feat: add bulma-carousel package, add bulma-carousel.js and use its in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~/plugins/vue-carousel.client.js'
+    '~/plugins/vue-carousel.client.js',
+    '~/plugins/bulma-carousel.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
---
+++ plugins/bulma-carousel.js
@@ -0,0 +1,4 @@
+import 'bulma-carousel/dist/css/bulma-carousel.min.css'
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
+
+export default bulmaCarousel;
